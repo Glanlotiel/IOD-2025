d35c34fedb4bdf3bc203eef66bfc3bf25ca42e55
Thomas Christopher Long <thomaschristopherlong@gmail.com>
2025-12-20 10:28:47 -0500

    Module 1 Lab exercises 2-4

---
+++ Module1/LabExercises/Exercise4.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    console.log(a + b)
+    console.log(a - b)
+    console.log(a / b)
+    console.log(a * b)
+}
+sum(2,3)
+
+greeting = "Hello, ";
+name = "Joe Smith";
+result = greeting + name;
+console.log(result);
