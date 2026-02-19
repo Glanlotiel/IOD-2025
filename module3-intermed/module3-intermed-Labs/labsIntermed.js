a0bfc161be00221ff62e7a22b70217dd005be94a
Thomas Christopher Long <thomaschristopherlong@gmail.com>
2026-02-18 19:51:24 -0500

    module intermed update

--- module3-intermed/module3-intermed-Labs/labsIntermed.js
+++ module3-intermed/module3-intermed-Labs/labsIntermed.js
@@ -349,7 +349,6 @@ function topEarner(salaries) {
   return maxName;
 }
 // --------------------------------------------------------------------------------------------------------------------- //
-
 /* The following code uses the Date object to print the current time and the number of hours
 that have passed today so far. Extend the code to do the following:
 
@@ -363,5 +362,43 @@ d) Write a function daysInBetween(date1, date2) which calculates and returns the
 amount of days in between the two given dates. */
 
 const today = new Date();
-console.log("Current time is " + today.toLocaleTimeString());
-console.log(today.getHours() + " hours have passed so far today");
+const Birthday = new Date("2001-5-27")
+
+const minutesToday = (today.getHours() * 60 + today.getMinutes())
+const secondsToday = (today.getHours() * 3600 + today.getMinutes() * 60 + today.getSeconds())
+// console.log("Current time is " + today.toLocaleTimeString());
+// console.log(today.getHours() + " hours have passed so far today");
+// console.log(minutesToday + " minutes have passed to far today")
+// console.log(secondsToday + " seconds have passed so far today.")
+
+let myAgeYear = today.getFullYear() - Birthday.getFullYear()
+let myAgeMonth = (today.getMonth() - Birthday.getMonth())
+let myAge = (today.getDate() - Birthday.getDate())
+
+// If days is negative, borrow from months
+if (myAge < 0) {
+    myAgeMonth--;
+    // Get the number of days in the previous month
+    const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    myAge += prevMonth.getDate();
+    }
+
+// If months is negative, borrow from years
+if (myAgeMonth < 0) {
+    myAgeYear--;
+    myAgeMonth += 12;
+}
+
+// console.log(`I am ${myAgeYear} years, ${myAgeMonth} months and ${myAge} days old`)
+
+
+function daysInbetween(date1, date2) {
+  const oneDay = 1000 * 60 * 60 * 24
+  const date1InMS = date1.getTime()
+  const date2InMS = date2.getTime()
+  return Math.round(Math.abs(date1InMS - date2InMS) / oneDay)
+}
+const date1 = new Date("2020-04-20")
+const date2 = new Date("2025-06-22")
+
+console.log(daysInbetween(date1, date2) + ' days inbetween ' + date1.toLocaleDateString() + ' and ' + date2.toLocaleDateString())
